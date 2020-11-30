715d66b28d2472c0054839de79549af1f35fbb2b
christine <christine@zipcodes-MacBook-Pro-10.local>
2020-11-30 17:29:53 -0500

    done

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -1,10 +1,8 @@
 package com.zipcodewilmington.arrayutility;
 
 import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * Created by leon on 3/6/18.
@@ -24,14 +22,38 @@ public class ArrayUtility<T> {
     }
 
     public T getMostCommonFromMerge(T[] arrayToMerge) {
-        return null;
+        List<T> aList = new ArrayList<>();
+        Map <T, Integer> numOfOccurrences = new HashMap<>();
+        aList.addAll(Arrays.asList(inputArray));
+        aList.addAll(Arrays.asList(arrayToMerge));
+        for (int i=0; i<aList.size(); i++){
+            Integer numberOfTimes = Collections.frequency(aList, aList.get(i));
+            numOfOccurrences.put(aList.get(i), numberOfTimes);
+        }
+        Map.Entry<T, Integer> mostCommon = null;
+        for (Map.Entry<T, Integer> e: numOfOccurrences.entrySet()){
+            if (mostCommon == null || mostCommon.getValue() < e.getValue()){
+                mostCommon = e;
+            }
+        }
+        return mostCommon.getKey();
     }
 
     public Integer getNumberOfOccurrences(T valueToEvaluate) {
-        return -1;
+        List<T> aList = new ArrayList<>();
+        aList.addAll(Arrays.asList(inputArray));
+        return Collections.frequency(aList, valueToEvaluate);
     }
 
     public T[] removeValue(T valueToRemove) {
-        return null;
+            int numberOfOccurrences = getNumberOfOccurrences(valueToRemove);
+            T[] result = Arrays.copyOf(inputArray, inputArray.length - numberOfOccurrences);
+            for (int i = 0, j = 0; i < inputArray.length; i++) {
+                if (inputArray[i] != valueToRemove) {
+                    result[j] = inputArray[i];
+                    j++;
+                }
+            }
+            return result;
     }
 }
