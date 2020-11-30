dbd59849eac0751a3f33ea7499ab4a1636327ff4
christine <christine@zipcodes-MacBook-Pro-10.local>
2020-11-30 11:11:22 -0500

    beginning groundwork

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -1,7 +1,26 @@
 package com.zipcodewilmington.arrayutility;
 
+import java.lang.reflect.Array;
+
 /**
  * Created by leon on 3/6/18.
  */
-public class ArrayUtility {
+public class ArrayUtility<T> {
+    ArrayUtility<T> array;
+
+    public ArrayUtility(T[] inputArray) {
+        this.array = new ArrayUtility<>(inputArray);
+    }
+
+    public Integer countDuplicatesInMerge(Integer[] arrayToMerge, Integer valueToEvaluate) {
+    }
+
+    public Integer getMostCommonFromMerge(Integer[] arrayToMerge) {
+    }
+
+    public Integer getNumberOfOccurrences(Integer valueToEvaluate) {
+    }
+
+    public Integer[] removeValue(Integer valueToRemove) {
+    }
 }
