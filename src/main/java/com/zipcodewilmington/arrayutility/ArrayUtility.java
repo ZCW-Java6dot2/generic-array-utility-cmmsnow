08565b9980fa11f2ed3c895fa146daecacd20d20
christine <christine@zipcodes-MacBook-Pro-10.local>
2020-11-30 13:12:47 -0500

    countDuplicatesInMerge working

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -1,26 +1,37 @@
 package com.zipcodewilmington.arrayutility;
 
 import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Created by leon on 3/6/18.
  */
 public class ArrayUtility<T> {
-    ArrayUtility<T> array;
+    private T[] inputArray;
 
     public ArrayUtility(T[] inputArray) {
-        this.array = new ArrayUtility<>(inputArray);
+        this.inputArray = inputArray;
     }
 
-    public Integer countDuplicatesInMerge(Integer[] arrayToMerge, Integer valueToEvaluate) {
+    public Integer countDuplicatesInMerge(T[] arrayToMerge, T valueToEvaluate) {
+        List<T> aList = new ArrayList<>();
+        aList.addAll(Arrays.asList(inputArray));
+        aList.addAll(Arrays.asList(arrayToMerge));
+        return Collections.frequency(aList, valueToEvaluate);
     }
 
-    public Integer getMostCommonFromMerge(Integer[] arrayToMerge) {
+    public T getMostCommonFromMerge(T[] arrayToMerge) {
+        return null;
     }
 
-    public Integer getNumberOfOccurrences(Integer valueToEvaluate) {
+    public Integer getNumberOfOccurrences(T valueToEvaluate) {
+        return -1;
     }
 
-    public Integer[] removeValue(Integer valueToRemove) {
+    public T[] removeValue(T valueToRemove) {
+        return null;
     }
 }
